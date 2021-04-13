2289cfbf0b6deabd122f5e7d501a2f88f3514636
maksim-ruksha <urpanium@gmail.com>
2021-04-13 12:38:38 +0300

    DAL progress (entities, generic repository)

---
+++ Cinema.DAL/Util/SittingPlaceType.cs
@@ -0,0 +1,29 @@
+﻿namespace Cinema.DAL.Entities
+{
+    public class SittingPlaceType
+    {
+        public readonly int Size;
+        public readonly bool IsVip;
+
+        public SittingPlaceType()
+        {
+            Size = 1;
+        }
+
+        public SittingPlaceType(int size)
+        {
+            Size = size;
+        }
+
+        public SittingPlaceType(bool isVip)
+        {
+            IsVip = isVip;
+        }
+
+        public SittingPlaceType(int size, bool isVip)
+        {
+            Size = size;
+            IsVip = isVip;
+        }
+    }
+}
