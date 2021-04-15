cb8a254f7e3500e04c6c13044dc11e60d23163f2
maksim-ruksha <urpanium@gmail.com>
2021-04-15 11:24:33 +0300

    Minor changes

--- Cinema.DAL/Entities/SittingPlace.cs
+++ Cinema.DAL/Entities/SittingPlace.cs
@@ -4,21 +4,10 @@ namespace Cinema.DAL.Entities
 {
     public class SittingPlace
     {
-        /*
-         * Position -------------------- X+
-         * |                             |
-         * |                             |
-         * |                             |
-         * |                             |
-         * |                             |
-         * |                             |
-         * Y+ ------------ Position + Size
-         */ 
-        
-        public Vector2 Position { get; set; }
-        public Vector2 Size { get; set; }
-        
-        public SittingPlaceType PlaceType { get; set; }
-        
+        public int Number;
+        public int Row;
+
+        public SittingPlaceType SittingPlaceType;
+
     }
 }
