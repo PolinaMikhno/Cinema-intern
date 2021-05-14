24f2753b03e0fce95ab24884e2a87ea035fdafbf
maksim-ruksha <urpanium@gmail.com>
2021-05-14 16:49:27 +0300

    New api methods

--- Cinema.API/Controllers/CinemaController.cs
+++ Cinema.API/Controllers/CinemaController.cs
@@ -1,17 +1,84 @@
-﻿using Cinema.DAL.EF;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Cinema.DAL.EF;
+using Cinema.DAL.Entities;
+using Cinema.Services.Models;
+using Cinema.Services.Services;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace Cinema.API.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
     public class CinemaController : Controller
     {
-        private readonly CinemaContext _context;
+        private readonly Service<FilmModel, FilmEntity> _filmService;
 
-        public CinemaController(CinemaContext context)
+        public CinemaController(CinemaContext context, IRepository<FilmEntity> repository, IMapper mapper)
         {
-            _context = context;
+            _filmService = new Service<FilmModel, FilmEntity>(repository, mapper);
         }
+
+        [HttpPost("/addFilms")]
+        public async Task<ActionResult> CreateFilms(IEnumerable<FilmModel> filmModels)
+        {
+            foreach (var filmModel in filmModels)
+            {
+                var result = await _filmService.CreateAsync(filmModel);
+                if (result == null)
+                    return BadRequest("Error while creating");
+            }
+
+            return Ok("Ok");
+        }
+
+        [HttpPost("/films")]
+        public async Task<IEnumerable<FilmModel>> GetFilms(string searchText, DateTime? start, DateTime? end)
+        {
+            if (start.HasValue && end.HasValue)
+            {
+                if (DateTime.Compare((DateTime) start, (DateTime) end) >= 0)
+                    throw new Exception("Start date is later than end date");
+            }
+
+            IEnumerable<FilmModel> films;
+            if (!string.IsNullOrWhiteSpace(searchText))
+            {
+                films = await _filmService.GetAsync(
+                    film =>
+                        (film.Name.Contains(searchText)
+                         || film.Description.Contains(searchText))
+                        && (start == null || DateTime.Compare(film.Start, (DateTime) start) >= 0)
+                        && (end == null || DateTime.Compare(film.End, (DateTime) end) <= 0)
+                        && (start == null || end == null ||
+                            Intersects(film.Start, film.End, (DateTime) start, (DateTime) end))
+                );
+            }
+            else
+            {
+                films = await _filmService.GetAsync();
+            }
+
+            return films;
+        }
+
+        private bool Intersects(DateTime dateTimeStart1, DateTime dateTimeEnd1, DateTime dateTimeStart2,
+            DateTime dateTimeEnd2)
+        {
+            return IsInsideRange(dateTimeStart1, dateTimeStart2, dateTimeEnd2)
+                   || IsInsideRange(dateTimeEnd1, dateTimeStart2, dateTimeEnd2)
+                   || IsInsideRange(dateTimeStart2, dateTimeStart1, dateTimeEnd1)
+                   || IsInsideRange(dateTimeEnd2, dateTimeStart1, dateTimeEnd1);
+        }
+
+        private bool IsInsideRange(DateTime dateTime, DateTime start, DateTime end)
+        {
+            return DateTime.Compare(dateTime, start) > 0
+                   && DateTime.Compare(dateTime, end) < 0;
+        }
+
+        //TODO: remove and edit films
     }
 }
